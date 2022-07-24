5525666b6498043a63eafc0b8eac93267bef0c5c
itchy-squid <86495347+itchy-squid@users.noreply.github.com>
2022-07-24 06:14:26 -0500

    Update scripts.js

--- set/js/scripts.js
+++ set/js/scripts.js
@@ -359,8 +359,8 @@ class Board {
         const padding = {
             left: 0,
             right: 0,
-            top: 30,
-            bottom: 30
+            top: 0,
+            bottom: 0
         };
 
         return {
